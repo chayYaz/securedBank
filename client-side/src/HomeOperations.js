67606dac32e67e084e595223bb7d635b0c683bab
chaya <chay7040@gmail.com>
2023-07-25 22:16:08 +0300

    adding getoperations main page

---
+++ client-side/src/HomeOperations.js
@@ -0,0 +1,58 @@
+import {
+  Link,
+  useParams,
+  useLocation,
+} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+const start = "http://localhost:3001/";
+export default function HomeOperations() {
+  const location = useLocation();
+
+  const [allOperations, setAllOperations] = useState([]);
+  
+  const [numOperations, setNumOperations] = useState(4);
+  const params = useParams();
+
+ 
+
+  const getOperations = async () => {
+    console.log("in get operation");
+    try {
+      let branch="Branch A";
+      let accountNum=1234567890;
+      let res = await fetch(
+        `${start}users/${accountNum}/${branch}/allOperations`
+      );
+      console.log(res.data);
+      let userOperations = await res.json();
+      setAllOperations(userOperations);
+      return;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getOperations();
+  }, []);
+
+  const addOperationsToDisplay = () => {
+    if(allOperations.length > numOperations) {
+      setNumOperations(prevNum => prevNum + 4);
+    }
+  };
+
+  return (
+    <div>
+      {allOperations.length === 0? 'Loading...' :
+        allOperations.slice(0, numOperations).map(operation => (
+        <>
+        <p>{operation.amount}</p>
+        <p>{operation.date}</p>
+        <p>{operation.receiver_account_number}</p>
+        </>))}
+      <p><Link onClick={addOperationsToDisplay} 
+        style={{display: numOperations >= allOperations.length && 'none'}}>More operation...</Link></p>
+    </div>
+  );
+}
---
+++ serverSide/serverHomeOperations.js
@@ -0,0 +1,43 @@
+
+/* The code above imports the necessary dependencies and sets up an
+ Express application with JSON parsing and CORS middleware. */
+// Importing required modules
+const express = require('express');// Express framework for building web applications
+const mysql2 = require('mysql2');// MySQL database driver
+const cors = require('cors');// Cross-Origin Resource Sharing middleware
+
+// Creating an instance of Express
+const app = express();
+app.use(express.json()); // Middleware to parse JSON data
+app.use(cors()); // Middleware for enabling Cross-Origin Resource Sharing
+
+app.get("/", (req, res) => {
+  console.log("Login route accessed");
+  res.send("Login page");
+});
+
+const connection = mysql2.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "0548574423",
+  database: "fullstack7",
+});
+
+// server starting
+app.listen(3001, () => {
+  console.log('Server started on port 3000.');
+});
+
+app.get("/users/:userNum/:branch/allOperations", (req, res) => {
+  console.log("in users/id");
+
+  const { userNum, branch } = req.params;
+  const query = "select * from operations where (sender_account_number=? and sender_branch=?) or(receiver_account_number=? and receiver_branch=?)";
+  let ansForQuery;
+  connection.query(query, [userNum, branch,userNum, branch], (err, results) => {
+    if (err) throw err;
+    ansForQuery = results;
+    console.log(ansForQuery);
+    res.send(ansForQuery);
+  });
+});
