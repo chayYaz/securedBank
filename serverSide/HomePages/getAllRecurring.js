6bdd1b0cb7bc7ad21f8c580bbc0f2da4921fc204
chaya <chay7040@gmail.com>
2023-08-09 19:33:08 +0300

    changing css

---
+++ serverSide/HomePages/getAllRecurring.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const connection = require('../database');
+
+router.get('/recurringTransfers', (req, res) => {
+  try {
+    // Query the database to get the recurring transfers data using callbacks
+    connection.query('SELECT * FROM recurring_transfers', (error, results) => {
+      if (error) {
+        console.error('Error getting recurring transfers:', error);
+        res.status(500).json({ message: 'Error getting recurring transfers' });
+      } else {
+        // Send the data as JSON response
+        res.json(results);
+      }
+    });
+  } catch (error) {
+    console.error('Error getting recurring transfers:', error);
+    res.status(500).json({ message: 'Error getting recurring transfers' });
+  }
+});
+module.exports = router;
